25e92f4f4ea33b1f4fca961974794094c9f89169
root <root@iZ23bcrbabkZ.(none)>
2014-10-11 13:42:59 +0800

    template commit

---
+++ upload/data/sysdata/cache_mobile.php
@@ -0,0 +1,6 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: b3661d193b66ad852fd802ab2a2bbdf9
+
+$mobilecheck = '{"discuzversion":"X3.2","charset":"utf-8","version":"4","pluginversion":"1.3.8","oemversion":"0","regname":"register","qqconnect":"0","sitename":"Mr.Bear","mysiteid":"37719526","ucenterurl":"http:\\/\\/121.199.30.154\\/wsq_t\\/upload\\/uc_server","setting":{"closeforumorderby":"0"},"extends":{"used":null,"lastupdate":null}}';
+?>
---
+++ upload/uc_server/data/cache/apps.php
@@ -0,0 +1,23 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://121.199.30.154/wsq_t/upload',
+    'authkey' => 'E9AdycN1mdbcE5T2B7b2qbTbfaH04521Bc1du3B7S2B9G2R5ub0dT523J7kfbe91',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '',
+    'allowips' => '',
+  ),
+);
+
+?>
