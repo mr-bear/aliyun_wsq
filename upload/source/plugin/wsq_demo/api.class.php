b8e316c840bee8245f463f69eed5783362897438
jsxiong110 <xiongfei@TN-10-20140168.tuniu-inc.com>
2014-10-13 17:34:28 +0800

    edit

--- upload/source/plugin/wsq_demo/api.class.php
+++ upload/source/plugin/wsq_demo/api.class.php
@@ -114,17 +114,57 @@ EOF;
 	}
 
 	function viewthread_postBottom() {
+        //done
+        global $_G;
+        require_once DISCUZ_ROOT.'./source/function/function_misc.php';
+        $positionImg = $_G['siteurl'].'./source/plugin/wsq_demo/1.jpg';
+
 		$return = array();
 		foreach($GLOBALS['postlist'] as $post) {
-			$return[$post['pid']] = '[postBottom/'.$post['pid'].']';
+            $userIp = $post['useip'];
+            $mobileType = intval($post['mobiletype']);
+            //convert ip to position
+            $convertRes = convertip($userIp);
+            if(is_string($convertRes)){
+                $convertRes = str_replace('-','',$convertRes);
+            }else{
+                $convertRes = '';
+            }
+            switch($mobileType){
+                case 1:
+                    $sourceName = 'iphone客户端';
+                    break;
+                case 2:
+                    $sourceName = 'android客户端';
+                    break;
+                case 3:
+                    $sourceName = 'windowsphone客户端';
+                    break;
+                case 5:
+                    $sourceName = '微社区';
+                    break;
+                default:
+                    $sourceName = 'pc端';
+                    break;
+            }
+
+            $posImgStyle = '<span style="background: url('.$positionImg.');width:15px;height:14px;margin-bottom:-3px;display:inline-block;"></span>';
+            $postionStyle = '<span style="line-height:24px;color:#bbb;font-size:11px;">'.$convertRes.'</span>';
+            $sourceStyle = '<span style="line-height:24px;margin-left:5px;color:#bbb;font-size:11px;">[来自<em style="color:#2d64b3;font-style: normal">'.$sourceName.'</em>]</span>';
+			$return[$post['pid']] = $posImgStyle.$postionStyle.$sourceStyle;
 		}
 		return $return;
 	}
 
 	function viewthread_authorInfo() {
 		$return = array();
+        $style = <<<EOF
+            <span class="gBg1 brSmall fb f8 c2" style="font-style:normal;">LV1</span>
+EOF;
 		foreach($GLOBALS['postlist'] as $post) {
-			$return[$post['authorid']] = '[authorInfo/'.$post['authorid'].']';
+            $itemStars = $post['stars'];
+            $style = '';
+			$return[$post['authorid']] = $style;
 		}
 		return $return;
 	}
@@ -134,21 +174,37 @@ EOF;
 	}
 
 	function viewthread_threadBottom() {
+        $current = Date('Y-m-d H:i:s');
+        $days = 3; // need set
+        $rule = 'replies';
+        $hotTime = strtotime($current) - 86400*$days;
+        $threadlist = C::t('forum_thread')->fetch_all_by_dateline($hotTime,0,3,$rule,'DESC');
+        $liStr = '';
+        $res = '';
+        if(!empty($threadlist)){
+            foreach($threadlist as $itemValue){
+                $subject = $this->pluginIconv($itemValue['subject']);
+                $liStr .= '<li style="line-height:22px;height:22px;font-size:12px;color:#b8b9ba;text-align:left;">·<a href="">'.$subject.'</a></li>';
+            }
 
-//        var_dump($GLOBALS['postlist']);
-//        echo 111;
-//		return json_encode($GLOBALS['postlist']);
-        return '[theadBottom]';
+            $spanStyle = '<span style="background-color:#f1f2f4;border-top:1px solid #ececec;border-bottom:1px solid #ececec;padding:10px;position:relative;display:block;">';
+            $spanTitle = '<span style="display: block;font-size:14px;color:#fff;font-weight:700;background:url(http://imgs.xici.net/_img/board/board_guess.gif) no-repeat;line-height:24px;height:24px;margin-left:-10px;margin-top:-10px;">热帖推荐</span>';
+            $contentStyle = '<ul style="width: 340px;">'.$liStr.'</ul>';
+            $res = $spanStyle.$spanTitle.$contentStyle.'</span>';
+        }
+
+        return $res;
 	}
 
 	function viewthread_topBar() {
-		return '[topBar]';
+//		return '[topBar]';
+        return json_encode($GLOBALS['postlist']);
 	}
 
 	//profile
 
 	function profile_authorInfo() {
-		return '[authorInfo]';
+		return '[profile_authorInfo]';
 	}
 
 	function profile_extraInfo() {
@@ -164,6 +220,16 @@ EOF;
 		return $return;
 	}
 
+    function pluginIconv($inData){
+        global $_G;
+        $charset = $_G['charset'];
+        $outData = $inData;
+        if('UTF-8' != $charset){
+            $outData = diconv($inData,$charset,'UTF-8');
+        }
+        return $outData;
+
+    }
 }
 
 ?>
