12f57903300b558eadb98be66fde40772d7bbf0e
jsxiong110 <xiongfei@TN-10-20140168.tuniu-inc.com>
2014-10-16 17:00:06 +0800

    edit

---
+++ upload/data/plugindata/mrbear_wsqdocjump.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiongfei
+ * Date: 14-10-16
+ * Time: 下午3:18
+ */
+
+$scriptlang['mrbear_wsqdocjump'] = array(
+    'pre' => '上一篇',
+    'next' => '下一篇',
+    'rand' => '随机看帖',
+    'randDetail' => '[点我随机看帖]开始未知之旅',
+);
+
