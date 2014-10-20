6b2b59c03058d136966281f50b9df18ad4454bb6
jsxiong110 <xiongfei@TN-10-20140168.tuniu-inc.com>
2014-10-20 18:07:48 +0800

    sofa edit

--- upload/source/plugin/mrbear_wsqsofa/view.inc.php
+++ upload/source/plugin/mrbear_wsqsofa/view.inc.php
@@ -3,16 +3,78 @@
 if (!defined('IN_DISCUZ')) {
     exit('Access Denied');
 }
+global $_G;
+//var_dump($_G);
+$isHistory = (isset($_GET['h']) && $_GET['h'] == 1) ? true : false;
+
+$rankTitle = ($isHistory) ? '历史排名' : '7天排名';
+
+$currentTime = date('Y-m-d');
+$unixTime = strtotime($currentTime);
+$unixTime = $unixTime - (3600*24*7);
+$rankTime = ($isHistory) ? '' : $unixTime;
+
+$siteUrl = $_G['siteurl'];
+$uid = $_G['uid'];
+$uname = $_G['username'];
+
+$sofaData = getSofaData($rankTime);
+
+$dom = '';
+if (!empty($sofaData)) {
+    $domStruct = <<<EOF
+    <dd _uid="9166708">
+        <span class="rbCon wot db">
+            <span class="rknums rknum{#rank#} br f11 c2 db">{#rank#}</span>
+
+            <img src="{#uimg#}" class="rbImg brBig db">
+            {#uname#}
+        </span>
+        <span class="rbNum db" style="display:inline">{#num#}</span>
+    </dd>
+EOF;
+    $i = 1;
+    foreach ($sofaData as $itemValue) {
+        $authorId = $itemValue['authorid'];
+        $author = $itemValue['author'];
+        $historyCount = $itemValue['count'];
+        $userImg = $siteUrl.'uc_server/avatar.php?uid='.$authorId.'&size=small';
+
+        $itemDom = str_replace('{#rank#}', $i, $domStruct);
+        $itemDom = str_replace('{#uname#}', $author, $itemDom);
+        $itemDom = str_replace('{#uimg#}', $userImg, $itemDom);
+        $itemDom = str_replace('{#num#}', $historyCount, $itemDom);
+        $dom .= $itemDom;
+        $i++;
+    }
+}
+
+//origin data
+function getSofaData($time = '')
+{
+    $historyCon = '';
+    if ($time != '') {
+        $historyCon = ' and dateline>='.$time;
+    }
+    $queryCon = 'SELECT authorid,author,count(1) count FROM '.DB::table('forum_post').' where position=2 '.$historyCon.' group by authorid order by count desc limit 10';
+
+    $historySofaData = DB::fetch_all($queryCon);
+    return $historySofaData;
+}
+
 
 ?>
+<html>
+<!--    <link rel="stylesheet" href="http://dzqun.gtimg.cn/c/1413637796=/quan/style/style.css,/quan/style/profile.css" onload="g_ts.css_end = new Date();" onerror="g_ts.css_end = new Date();">-->
+<link rel="stylesheet" href="http://121.199.30.154/wsq_t/upload/source/plugin/mrbear_wsqsofa/style.css"">
 <script type="text/javascript" src="http://wsq.discuz.qq.com/cdn/discuz/js/openjs.js"></script>
 <script>
     var menu = new Array();
-    menu.push({name:"menu1", pluginid:'wsq_demo:view', param:"a=1&b=2"});
-    menu.push({name:"menu2", pluginid:'wsq_demo:view', param:"a=3&b=4"});
+    menu.push({name:"7天排名", pluginid:'mrbear_wsqsofa:view', param:""});
+    menu.push({name:"历史排名", pluginid:'mrbear_wsqsofa:view', param:"h=1"});
     WSQ.initBtmBar(menu);
     WSQ.showBtmBar();
-    WSQ.initPlugin({name:''});
+    WSQ.initPlugin({name:'1111'});
 
     var initWx = {
         'img': 'http://www.discuz.net/static/image/common/logo.png',
@@ -23,34 +85,37 @@ if (!defined('IN_DISCUZ')) {
     };
     WSQ.initShareWx(initWx);
 </script>
-<style type="text/css">
-    div {display: block;}
-    .mod-lists {margin: 0px 10px 0 0px;background: #fafafa;}
-    .hd {margin-bottom: 0;display: -webkit-box;}
-    .hd h2 {-webkit-box-flex: 1;margin: 0;padding: 0;font-size: 18px;}
-    .bars {display: -webkit-box;}
-    .on {color: #299DE7;background: url("http://mu5.bdstatic.com/st/img/app/home/24/tab_indicator.png") bottom center no-repeat;background-size: 19px 10px;padding: 18px 0 12px 0;font-size: 16px;-webkit-box-flex: 1;text-align: center;position: relative;z-index: 2;}
-    i {font-style: normal;}
-    .gap-line {position: absolute;right: 0;top: 20px;height: 18px;width: 1px;border-left: 1px solid #eee}
-    .bar {padding: 18px 0 12px 0;font-size: 16px;-webkit-box-flex: 1;text-align: center;color: #999;position: relative;z-index: 2;}
 
+<body style="zoom: 1; padding-bottom: 55px;">
+<div class="warp">
+    <div class="rankBox">
+        <dl>
+            <dt class="pr">
+<!--                还有10个帖子没有评论-->
+<!--                <span style="background-color: #ffa903;display: inline-block;right: 50px">-->
+<!--                    点我抢-->
+<!--                </span>-->
 
+                <span class="rbName f14 c9 db" style="width: 70%">还有10个帖子没有评论</span>
+                <span style="background-color: #ffa903;display: inline-block;color: #fff;">
+                    点我开抢喽！
+                </span>
 
-</style>
-</head>
-<body>
+            </dt>
+        </dl>
+        <dl>
+            <dt class="pr">
+                <span class="honorBg brSmall pa">
+                    <i class="iconRank db cf f10 c2"></i>
+                </span>
+                <span class="rbName f14 c9 db"><?php echo $rankTitle ?></span><span class="rbNum f14 c9 db" style="text-align:left;">沙发数</span>
+            </dt>
 
-<div class="mod-lists ">
-    <div class="hd" >
-        <h2>音乐榜单</h2>
+            <?php echo $dom; ?>
+
+        </dl>
     </div>
-    <div class="bars">
-        <div class="on">热歌榜<i></i><span class="gap-line"> </span></div>
-        <div class="bar">新歌榜<i></i></div>
-    </div>
-
-
 </div>
 
+</body></html>
 
-</body>
