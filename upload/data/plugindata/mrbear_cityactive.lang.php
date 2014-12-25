952f5a4d5d521c50de14a9714bde175e72b8ed1d
jsxiong110 <xiongfei@TN-10-20140168.tuniu-inc.com>
2014-12-25 18:03:34 +0800

    edit

---
+++ upload/data/plugindata/mrbear_cityactive.lang.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiongfei
+ * Date: 14-12-25
+ * Time: 下午2:11
+ */
+
+$templatelang['mrbear_cityactive'] = array(
+    'main_hotcity' => '热门城市',
+    'main_label_all' => '全部',
+    'main_label_fee' => '费用',
+    'main_label_fee_free' => '免费',
+    'main_label_fee_self' => '自费',
+    'main_label_order' => '排序',
+    'main_label_order_praise' => '关注度',
+    'main_label_order_adtime' => '发布时间',
+    'main_label_order_acnum' => '参与人数',
+    'main_detail_address' => '地点',
+    'main_detail_time' => '时间',
+    'main_detail_fee' => '票价',
+    'main_detail_tag' => '标签',
+    'main_page_first' => '首页',
+    'main_page_pre' => '上一页',
+    'main_page_next' => '下一页',
+    'main_page_last' => '尾页',
+    'main_label_myact' => '我的活动',
+    'main_label_myact_nb' => '未开始',
+    'main_label_myact_b' => '进行中',
+    'main_label_myact_a' => '已报名',
+    'main_label_myact_p' => '感兴趣',
+    'main_label_mostact' => '人气活动',
+    'main_label_nb' => '即将开始',
+
+);
+
+$scriptlang['mrbear_cityactive'] = array();
